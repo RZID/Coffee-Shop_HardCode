071a7688bc8eb6ccd0f536c95663b751dbf15506
FoXyKinGs <mochrizaldianandakurnia21@gmail.com>
2021-02-14 21:36:36 +0700

    Layout History

--- src/views/History.vue
+++ src/views/History.vue
@@ -7,25 +7,366 @@
           <h2>Let's see what you have bought</h2>
           <p>Select item to delete</p>
         </div>
-        <div>Select All</div>
-        <div class="row">
-          <div class="col-4 bg-white rounded">
-            <div class="row">
-              <div class="col"></div>
-              <div class="col"></div>
+        <div class="text-white">Select All</div>
+        <div class="content">
+          <div class="row p-0 m-0">
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
             </div>
+            <!-- Hapus -->
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="../assets/image 43.png" alt="">
+                </div>
+                <div class="col-lg-8">
+                  <div class="product-name">
+                  <p style="font-size:15px;">Nasi Goreng Bumbu Pedas</p>
+                  </div>
+                  <hr>
+                  <div class="information">
+                    <p style="font-size: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi cumque debitis ipsam omnis ratione ipsa dicta consequuntur illum porro sit.</p>
+                  </div>
+                  <input type="checkbox">
+                </div>
+              </div>
+            </div>
+            <!-- Selesai -->
           </div>
         </div>
       </div>
     </div>
+    <Footer />
   </div>
 </template>
 <script>
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 import Axios from 'axios'
 export default {
   components: {
-    Navbar
+    Navbar,
+    Footer
   },
   mounted () {
     Axios.get(`${process.env.VUE_APP_BACKEND}/api/history/${this.$store.getters['auth/getUserData'].uid}`, {
@@ -42,11 +383,44 @@ export default {
 </script>
 <style scoped>
 .bg-coffee {
-  height: 100vh;
+  height: 900px;
   width: 100%;
   background-image: url("/image/tyler-nix-jE-27l7V3NU-unsplash.jpg");
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+}
+.content{
+  width: 100%;
+  height: 700px;
+  overflow: auto;
+}
+.content .row .col-lg-3{
+  margin-top: 20px;
+}
+.content .row .col-lg-3 .row{
+  width: 100%;
+  border-radius: 25px;
+  background: white;
+}
+.content .row .col-lg-4 img{
+  width: 70px;
+  height: 125px;
+}
+.content .row .col-lg-8 .information{
+  width: 100%;
+  height: 50px;
+  overflow: auto;
+}
+.content .row .col-lg-8 .product-name{
+  width: 100%;
+  height: 50px;
+  overflow: auto;
+  margin-top: 10px;
+  margin-bottom: -10px;
+}
+.content .row .col-lg-8 input{
+  position: relative;
+  right: -80%;
 }
 </style>
